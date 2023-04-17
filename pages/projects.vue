75174844c805c2b44545186963a1f7e260caf7d1
Samet <sametkeles@outlook.com>
2023-04-17 15:06:46 +0300

    Coded the general structure of the projects page

--- pages/projects.vue
+++ pages/projects.vue
@@ -4,33 +4,126 @@
       <h1 class="projects__title">Projects</h1>
       <div class="cards">
         <div class="card">
-          <img src="/img/todo-list.webp" alt="" class="card__img" />
-          <div class="card__text">
-            <h3 class="card__title">To Do List</h3>
-            <p class="card__description">
-              Html, Css, Bootstrap, Javascript todo list çalışması
-            </p>
-          </div>
+          <a
+            href="https://github.com/samet-keles/JavaScrit-ToDoList"
+            target="_blank"
+          >
+            <img src="/img/todo-list.webp" alt="" class="card__img" />
+            <div class="card__text">
+              <h3 class="card__title">To Do List</h3>
+              <p class="card__description">
+                Html, Css, Bootstrap, Javascript todo list çalışması
+              </p>
+            </div>
+          </a>
         </div>
-        <!-- <div class="card"></div>
-        <div class="card"></div>
-        <div class="card"></div>
-        <div class="card"></div> -->
+
+        <div class="card">
+          <a
+            href="https://github.com/samet-keles/JavaScrit-ToDoList"
+            target="_blank"
+          >
+            <img src="/img/todo-list.webp" alt="" class="card__img" />
+            <div class="card__text">
+              <h3 class="card__title">To Do List</h3>
+              <p class="card__description">
+                Html, Css, Bootstrap, Javascript todo list çalışması
+              </p>
+            </div>
+          </a>
+        </div>
+
+        <div class="card">
+          <a
+            href="https://github.com/samet-keles/JavaScrit-ToDoList"
+            target="_blank"
+          >
+            <img src="/img/todo-list.webp" alt="" class="card__img" />
+            <div class="card__text">
+              <h3 class="card__title">To Do List</h3>
+              <p class="card__description">
+                Html, Css, Bootstrap, Javascript todo list çalışması
+              </p>
+            </div>
+          </a>
+        </div>
+
+        <div class="card">
+          <a
+            href="https://github.com/samet-keles/JavaScrit-ToDoList"
+            target="_blank"
+          >
+            <img src="/img/todo-list.webp" alt="" class="card__img" />
+            <div class="card__text">
+              <h3 class="card__title">To Do List</h3>
+              <p class="card__description">
+                Html, Css, Bootstrap, Javascript todo list çalışması
+              </p>
+            </div>
+          </a>
+        </div>
+
+        <div class="card">
+          <a
+            href="https://github.com/samet-keles/JavaScrit-ToDoList"
+            target="_blank"
+          >
+            <img src="/img/todo-list.webp" alt="" class="card__img" />
+            <div class="card__text">
+              <h3 class="card__title">To Do List</h3>
+              <p class="card__description">
+                Html, Css, Bootstrap, Javascript todo list çalışması
+              </p>
+            </div>
+          </a>
+        </div>
+
+        <div class="card">
+          <a
+            href="https://github.com/samet-keles/JavaScrit-ToDoList"
+            target="_blank"
+          >
+            <img src="/img/todo-list.webp" alt="" class="card__img" />
+            <div class="card__text">
+              <h3 class="card__title">To Do List</h3>
+              <p class="card__description">
+                Html, Css, Bootstrap, Javascript todo list çalışması
+              </p>
+            </div>
+          </a>
+        </div>
       </div>
     </div>
   </section>
 </template>
 
 <style lang="scss" scoped>
-.projects {
-  .cards {
-    margin-top: 5rem;
-    .card {
-      width: 350px;
-      outline: 1px solid rgb(78, 78, 78);
+@import "@/assets/scss/config/variables";
 
+.projects {
+  padding-top: 1rem;
+  padding-bottom: 5rem;
+  &__title {
+    padding-bottom: 5rem;
+    text-align: center;
+    font-size: 3.6rem;
+
+    .dark & {
+      color: white;
+    }
+  }
+  .cards {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    gap: 4rem;
+    .card {
+      width: 27.5%;
+      // width: 31.09%;
+      border-radius: 12px;
+      outline: 1px solid $gray;
       &:hover {
-        outline: 2px solid purple;
+        outline: 2px solid $purple;
       }
 
       &__img {
@@ -42,7 +135,7 @@
       &__text {
         padding: 2rem;
         width: 100%;
-        background-color: rgb(44, 44, 44);
+        background-color: $dark-gray;
         border-bottom-right-radius: 12px;
         border-bottom-left-radius: 12px;
       }
@@ -54,7 +147,7 @@
       }
 
       &__description {
-        color: rgb(196, 196, 196);
+        color: $light-gray;
         font-size: 12px;
         font-weight: 400;
       }
