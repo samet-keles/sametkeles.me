49f75005de4dd31dda94cefc61bdd07fcba0954e
Samet <sametkeles@outlook.com>
2023-08-16 11:56:41 +0300

    fixed bug on projects page

--- composables/useProjects.js
+++ composables/useProjects.js
@@ -2,12 +2,12 @@ import { ref } from "vue";
 import { db } from "@/db";
 import { collection, onSnapshot } from "firebase/firestore";
 
-const UseProjectsArr = ref([]);
+export const useProjectsArr = ref([]);
 
 const projectsCollection = collection(db, "projects");
 
 export function useProjectsData() {
   return onSnapshot(projectsCollection, (snapshot) => {
-    UseProjectsArr.value = snapshot.docs.map((project) => project.data());
+    useProjectsArr.value = snapshot.docs.map((project) => project.data());
   });
 }
